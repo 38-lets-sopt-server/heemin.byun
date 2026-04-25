cd2e9e1369afa700810fa5d5cca86361e2e54af7
byunhm02 <byunhm02@gmail.com>
2026-04-25 13:32:20 +0900

    refactor: 스웨거와 이름 충돌 방지를 위한 네이밍 변경

--- src/main/java/org/sopt/post/controller/PostController.java
+++ src/main/java/org/sopt/post/controller/PostController.java
@@ -1,12 +1,10 @@
 package org.sopt.post.controller;
 
 import org.sopt.global.api.code.PostSuccessCode;
-import org.sopt.global.api.exception.BaseException;
 import org.sopt.post.dto.request.CreatePostRequest;
 import org.sopt.post.dto.request.UpdatePostRequest;
 import org.sopt.post.dto.response.PostResponse;
-import org.sopt.post.exception.PostNotFoundException;
-import org.sopt.global.api.response.ApiResponse;
+import org.sopt.global.api.response.BaseResponse;
 import org.sopt.post.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,45 +24,45 @@ public class PostController {
 
     // POST /posts
     @PostMapping
-    public ResponseEntity<ApiResponse<PostResponse>> createPost(@RequestBody CreatePostRequest request) {
+    public ResponseEntity<BaseResponse<PostResponse>> createPost(@RequestBody CreatePostRequest request) {
         PostResponse post = postService.createPost(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(ApiResponse.success(PostSuccessCode.POST_CREATED, post));
+                .body(BaseResponse.success(PostSuccessCode.POST_CREATED, post));
     }
 
     // GET /posts 📝 과제
     @GetMapping
-    public ResponseEntity<ApiResponse<List<PostResponse>>> getAllPosts() {
+    public ResponseEntity<BaseResponse<List<PostResponse>>> getAllPosts() {
         // TODO: postService.getAllPosts() 호출해서 반환
         List<PostResponse> posts = postService.readAllPosts();
-        return ResponseEntity.ok(ApiResponse.success(PostSuccessCode.POST_LIST_READ, posts));
+        return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_LIST_READ, posts));
     }
 
     // GET /posts/{id} 📝 과제
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<PostResponse>> getPost(
+    public ResponseEntity<BaseResponse<PostResponse>> getPost(
             @PathVariable final Long id) {
         // TODO: postService.getPost(id) 호출, 예외 발생 시 null 반환
         PostResponse post = postService.readPost(id);
-        return ResponseEntity.ok(ApiResponse.success(PostSuccessCode.POST_READ, post));
+        return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_READ, post));
     }
 
     // PUT /posts/{id} 📝 과제
     @PutMapping("{id}")
-    public ResponseEntity<ApiResponse<PostResponse>> updatePost(
+    public ResponseEntity<BaseResponse<PostResponse>> updatePost(
             @PathVariable final Long id,
             @RequestBody UpdatePostRequest request) {
         // TODO: postService.updatePost() 호출, 예외 발생 시 에러 메시지 출력
         PostResponse post = postService.updatePost(id, request);
-        return ResponseEntity.ok(ApiResponse.success(PostSuccessCode.POST_UPDATED, post));
+        return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_UPDATED, post));
     }
 
     // DELETE /posts/{id} 📝 과제
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable final Long id) {
+    public ResponseEntity<BaseResponse<Void>> deletePost(@PathVariable final Long id) {
         // TODO: postService.deletePost() 호출, 예외 발생 시 에러 메시지 출력
         postService.deletePost(id);
-        return ResponseEntity.ok(ApiResponse.success(PostSuccessCode.POST_DELETED));
+        return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_DELETED));
     }
 }

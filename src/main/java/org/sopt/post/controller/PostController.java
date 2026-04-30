e6393f4a2dd7bc465b586c035f33116cbbe26b44
byunhm02 <byunhm02@gmail.com>
2026-04-30 13:49:17 +0900

    feat: 스웨거 설정 적용 및 엔티티로 변경

--- src/main/java/org/sopt/post/controller/PostController.java
+++ src/main/java/org/sopt/post/controller/PostController.java
@@ -1,8 +1,12 @@
 package org.sopt.post.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.sopt.global.api.code.PostSuccessCode;
 import org.sopt.post.dto.request.CreatePostRequest;
 import org.sopt.post.dto.request.UpdatePostRequest;
+import org.sopt.post.dto.response.PostListResponse;
 import org.sopt.post.dto.response.PostResponse;
 import org.sopt.global.api.response.BaseResponse;
 import org.sopt.post.service.PostService;
@@ -14,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
+@Tag(name = "게시글 작성",description = "게시글 작성,수정,조회,삭제 관련 API")
 public class PostController {
 
     private final PostService postService;
@@ -22,8 +27,8 @@ public class PostController {
         this.postService = postService;
     }
 
-    // POST /posts
     @PostMapping
+    @Operation(summary = "게시글 작성" ,description = "멤버id를 받아 게시글을 작성합니다.")
     public ResponseEntity<BaseResponse<PostResponse>> createPost(@RequestBody CreatePostRequest request) {
         PostResponse post = postService.createPost(request);
         return ResponseEntity
@@ -31,41 +36,38 @@ public class PostController {
                 .body(BaseResponse.success(PostSuccessCode.POST_CREATED, post));
     }
 
-    // GET /posts 📝 과제
     @GetMapping
-    public ResponseEntity<BaseResponse<List<PostResponse>>> getAllPosts() {
-        // TODO: postService.getAllPosts() 호출해서 반환
-        List<PostResponse> posts = postService.readAllPosts();
+    @Operation(summary = "게시글 전체 조회" ,description = "전체 게시글 리스트를 반환합니다.")
+    public ResponseEntity<BaseResponse<List<PostListResponse>>> getAllPosts() {
+        List<PostListResponse> posts = postService.readAllPosts();
         return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_LIST_READ, posts));
     }
 
-    // GET /posts/{id} 📝 과제
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 단건 조회" ,description = "단건 게시글을 조회합니다.")
     public ResponseEntity<BaseResponse<PostResponse>> getPost(
+            @Parameter(description = "조회할 게시글의 ID", example = "1", required = true)
             @PathVariable final Long id) {
-        // TODO: postService.getPost(id) 호출, 예외 발생 시 null 반환
         PostResponse post = postService.readPost(id);
         return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_READ, post));
     }
 
-    // PUT /posts/{id} 📝 과제
     @PutMapping("{id}")
+    @Operation(summary = "게시글 수정" ,description = "존재하던 게시글을 수정합니다.")
     public ResponseEntity<BaseResponse<PostResponse>> updatePost(
+            @Parameter(description = "조회할 게시글의 ID", example = "1", required = true)
             @PathVariable final Long id,
             @RequestBody UpdatePostRequest request) {
-        // TODO: postService.updatePost() 호출, 예외 발생 시 에러 메시지 출력
         PostResponse post = postService.updatePost(id, request);
         return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_UPDATED, post));
     }
 
-    /*
-    // DELETE /posts/{id} 📝 과제
     @DeleteMapping("/{id}")
-    public ResponseEntity<BaseResponse<Void>> deletePost(@PathVariable final Long id) {
-        // TODO: postService.deletePost() 호출, 예외 발생 시 에러 메시지 출력
+    @Operation(summary = "게시글 삭제" ,description = "게시글을 삭제합니다.")
+    public ResponseEntity<BaseResponse<Void>> deletePost(
+            @Parameter(description = "조회할 게시글의 ID", example = "1", required = true)
+            @PathVariable final Long id) {
         postService.deletePost(id);
         return ResponseEntity.ok(BaseResponse.success(PostSuccessCode.POST_DELETED));
     }
-
-     */
 }
--- src/main/java/org/sopt/post/domain/Post.java
+++ src/main/java/org/sopt/post/domain/Post.java
@@ -1,17 +1,22 @@
 package org.sopt.post.domain;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.sopt.global.api.domain.BaseEntity;
 import org.sopt.user.domain.User;
 
 import java.time.LocalDateTime;
 
+@SQLDelete(sql = "UPDATE post SET deleted_at = NOW() WHERE id = ?")
+@Where(clause = "deleted_at IS NULL")
 @Entity
-public class Post {
+public class Post extends BaseEntity {
 
     @Id // 앞에서 배운 PK
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // 게시글 상세 화면 — 특정 게시글 식별용
+
     private String title;     // 목록, 상세, 글쓰기 화면 — 제목
     private String content;   // 목록(미리보기), 상세(전체) 화면 — 내용
 
@@ -19,26 +24,25 @@ public class Post {
     @JoinColumn(name = "user_id")       // post 테이블에 user_id FK 컬럼이 생겨요
     private User user;
 
-    private String author;    // 목록, 상세 화면 — 글쓴이
-    private LocalDateTime createdAt; // 목록, 상세 화면 — 작성 시각
-
-    public Post(Long id, String title, String content, String author, LocalDateTime createdAt) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.author = author;
-        this.createdAt = createdAt;
-    }
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     public Long getId() { return id; }
     public String getTitle() { return title; }
     public String getContent() { return content; }
-    public String getAuthor() { return author; }
-    public LocalDateTime getCreatedAt() { return createdAt; }
+    public User getUser() { return user; }
 
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return deletedAt != null;
     }
 
     protected Post() {}  // JPA 기본 생성자

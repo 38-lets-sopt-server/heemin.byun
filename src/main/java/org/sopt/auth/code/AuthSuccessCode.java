dfa734af53a57ecf62b9fca900800f0fb2273768
byunhm02 <byunhm02@gmail.com>
2026-05-20 15:12:35 +0900

    feat:auth 성공 및 에러 코드 정의

--- src/main/java/org/sopt/auth/code/AuthSuccessCode.java
+++ src/main/java/org/sopt/auth/code/AuthSuccessCode.java
@@ -6,8 +6,13 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum AuthSuccessCode implements ResponseCode {
+    LOGIN_SUCCEED(HttpStatus.OK,"로그인 성공!"),
+    KAKAO_LOGIN_SUCCEED(HttpStatus.OK,"카카오 로그인 성공!"),
+    SIGNUP_SUCCEED(HttpStatus.OK,"회원가입 성공!"),
     TOKEN_GENERATED(HttpStatus.CREATED, "토큰 발급 완료!"),
-    USER_SEARCHED(HttpStatus.OK,"유저 조회 완료!");
+    USER_SEARCHED(HttpStatus.OK,"유저 조회 완료!"),
+    LOGOUT_SUCCESS(HttpStatus.OK,"로그아웃 성공!")
+    ;
 
     private final HttpStatus status;
     private final String message;
---
+++ src/main/java/org/sopt/auth/exception/code/AuthErrorCode.java
@@ -0,0 +1,28 @@
+package org.sopt.auth.exception.code;
+
+import org.sopt.global.api.code.ResponseCode;
+import org.springframework.http.HttpStatus;
+
+public enum AuthErrorCode implements ResponseCode {
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    KAKAO_USER_CANNOT_LOGIN(HttpStatus.BAD_REQUEST, "카카오로 가입한 유저입니다. 카카오 로그인을 이용해주세요."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    SIGNUP_REQUIRED(HttpStatus.NOT_FOUND,"회원가입이 필요합니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    AuthErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getStatus() { return status; }
+
+    @Override
+    public String getMessage() { return message; }
+}
